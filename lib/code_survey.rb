fd3764f65446ecc96c95a637444bbb5d0ad819cc
Andrei Nagy <nagy.andrei@gmail.com>
2019-02-02 12:54:18 +0200

    Initial commit.

---
+++ lib/code_survey.rb
@@ -0,0 +1,7 @@
+require 'code_survey/version'
+
+module Main
+  def self.main(_argv)
+    puts 'Main'
+  end
+end
---
+++ test/test_helper.rb
@@ -0,0 +1,4 @@
+$LOAD_PATH.unshift File.expand_path('../lib', __dir__)
+require 'code_survey'
+
+require 'minitest/autorun'
